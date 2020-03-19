397185fa0fb261c916a13f0812c40070e02fd568
DavidCARTIERE <cartiere@hotmail.fr>
2020-03-20 00:41:40 +0100

    bientot fini

--- scripts.js
+++ scripts.js
@@ -1,227 +1,366 @@
 // Main program 
 $(document).ready(function() {
 
+    var n = get_n();
+    var m = get_m();
+
+    buildZVariables(n);
+    buildGridVariables(n,m);
 
     $("#findSimplex").click(function() {
-        calculCj(); // retourne premier critère 
-        minRatio(); // retourne deuxieme critere 
-        buildBaseTable(); // retourne tableau de travail 
-        // main();
 
-        console.log(calculCj());
-        console.log(minRatio());
-        console.log(buildBaseTable());
-        var grille_base = buildBaseTable();
-        console.log(getColonneQ(grille_base));
+        var n = get_n();
+        var m = get_m();
+
+        var cpTab = generateCpTab(m);
+        var gridTab = generateGridTab(n,m);
+        var cjTab = generateCjTab(n,m);
+        var qTab = generateQTab(m);
+        
+       
+        // var qTab = ["18","42","24"];
+        // var gridTab = [
+        //     ["2","1"],
+        //     ["2","3"],
+        //     ["3","1"],
+        // ];
+        // var cjTab = ["3","2","0","0","0"];
+
+        console.log("cp",cpTab);
+        console.log("gridTab",gridTab);
+        console.log("cjTab",cjTab);
+      
+
+        
+        var outpoutVariablesTab = generateOutputVariables(n, m);
+       
+        var gridTab = addOutputVariables(gridTab, n, m, outpoutVariablesTab);
+        var index_of_first_critere = findFirstCritere(cjTab);
+        var index_of_second_critere = findSecondCritere(qTab, gridTab, index_of_first_critere, m);
+        var valueOfPivot = getPivotValue(gridTab, index_of_first_critere, index_of_second_critere );
+        var cpXjTab = generateCpXjTab(cpTab, gridTab,n,m);
+        cpTab = updateCpTab(cpTab, index_of_second_critere, valueOfPivot);
+        /* Changer les valeurs des lignes ici --> Faire la fonction) */
+        generateCpXjTab(cpTab, gridTab,n,m);
+        var cpZjTab = generateCpZjTab(cjTab, cpXjTab, m,n);
+
+        if(verifyValuesOfCpCz(cpZjTab,n,m)){
+            gridTab = majAlltabs(gridTab, index_of_second_critere, index_of_first_critere, valueOfPivot, m,n);
+            gridTab = majOthersLines(gridTab, index_of_second_critere, index_of_first_critere,valueOfPivot, m,n);
+        }
+       
+
+       
+
+      
+       
+       
+        
+        
+        
+     });
+
+    $("#validate_param").click(function() {
+
+        var n = get_n();
+        var m = get_m();
+
+        buildZVariables(n);
+        buildGridVariables(n,m); 
+       
     });
-
-
 });
 
-// ---- fonction qui appelle les fonctions après la validation du nombre de contraintes et de variables 
-function checkscript() {
+//---------------- FONCTIONS POUR LA GENERATION DE HTML ----------------------------- // 
 
-    getParametersBuildSelection();
-    buildZVariables();
-
-}
-
-// End Main Program !
-
-
-
-// ---- récupére les valeurs 
-//Créer le formulaire de saisie des contraintes 
-function getParametersBuildSelection() {
-
-    $("#div_contraintes").empty();
-
-    var nbVariables = $("#txtNbVariables").val();
-    var nbContraintes = $("#txtNbContraintes").val();
-    var i = 0;
-    var j = 0;
-
-    for (i = 0; i < nbContraintes; i++) {
-        var txt0 = "";
-        txt0 += "<p>Contraintes " + i + ":</p>";
-        $("#div_contraintes").append(txt0);
-
-
-        for (j = 0; j < nbVariables; j++) {
-            var txt = "<input id='txtCj" + i + "_" + j + "' type='text' size='2'>";
-
-            txt += "<label for='txtCj" + i + "_" + j + "'>x<sub>" + (j + 1) + "</sub></label>";
-            $("#div_contraintes").append(txt);
-        }
-
-
-        var txt1 = "";
-        txt1 += "<select id='selection_comparateur" + i + "'>";
-        txt1 += "<option id='comparateurInfEgale'><=</option>";
-        txt1 += "<option id='comparateurSupEgale'>=></option>";
-        txt1 += "<option id='comparateurEgale'>=</option>";
-        txt1 += '</select>';
-        txt1 += "<input type='text' id='valeur_contrainte_" + i + "'  size='2'>";
-
-        $("#div_contraintes").append(txt1);
-    }
-
-    $(".hidden_content").addClass("visible_content");
-    $(".visible_content").removeClass("hidden_content");
-
-}
-
-//---- Créer le formulaire pour saisir le Z = 
-function buildZVariables() {
+// 1) Créer le formulaire pour saisir le Z = 
+// n = nombre de variables 
+function buildZVariables(n) {
 
     $("#div_valeur_z").empty();
 
-    var nbVariables = $("#txtNbVariables").val();
-
-    for (j = 0; j < nbVariables; j++) {
-        var create_z = "<input id='variable_z" + j + "' type='text' size='2'>";
-        create_z += "<label for='variable_z" + j + "'>x<sub>" + (j + 1) + "</sub></label>";
+    for (var i = 0; i < n; i++){
+        var create_z = "<input id='variable_z" + i + "' type='text' size='2'>";
+        create_z += "<label for='variable_z" + i + "'>x<sub>" + (i + 1) + "</sub></label>";
         $("#div_valeur_z").append(create_z);
     }
+    
 }
 
-// ---- Calcule du critere 1 
-function calculCj() {
+// 2) Créer le formulaire de saisie des contraintes 
+function buildGridVariables(n,m) {
 
-    var nbVariables = $("#txtNbVariables").val();
-    var nbContraintes = $("#txtNbContraintes").val();
-    var tab_Cj = [];
-    var i, j;
-    // var value_operateur;
+    $("#div_contraintes").empty();
 
-    for (j = 0; j < nbVariables; j++) {
-        tab_Cj.push($("#variable_z" + j + "").val());
-    }
+    for (var i = 0; i < m; i++) {
+            var txt0 = "";
+            txt0 += "<p>Contraintes " + i + ":</p>";
+            $("#div_contraintes").append(txt0);
 
-    for (i = 0; i < nbContraintes; i++) {
-        if ($("#selection_comparateur" + i + " option:selected").text() == "<=") {
-            tab_Cj.push("0");
+            for (var j = 0; j < n; j++) {
+                    var txt = "<input id='txtCj" + i + "_" + j + "' type='text' size='2'>";
+                    txt += "<label for='txtCj" + i + "_" + j + "'>x<sub>" + (j + 1) + "</sub></label>";
+                    $("#div_contraintes").append(txt);
+            }
+
+            var txt1 = "";
+            txt1 += "<select id='selection_comparateur" + i + "'>";
+            txt1 += "<option id='comparateurInfEgale'><=</option>";
+            txt1 += "<option id='comparateurSupEgale'>=></option>";
+            txt1 += "<option id='comparateurEgale'>=</option>";
+            txt1 += '</select>';
+            txt1 += "<input type='text' id='valeur_contrainte_" + i + "'  size='2'>";
+
+            $("#div_contraintes").append(txt1);
+        }
+        
+    $(".hidden_content").addClass("visible_content");
+    $(".visible_content").removeClass("hidden_content");
+}
+
+
+
+
+//---------------- FONCTIONS POUR LE TRAITEMENT         ----------------------------- // 
+// 1) Récupère le nombre de variables
+function get_n(){
+    return $("#txtn").val();
+}
+
+// 2) Récupère le nombre de m 
+function get_m(){
+    return $("#txtm").val();
+}
+
+
+//---------------- FONCTIONS POUR BUILD LES TABLES DE BASES ------------------------ //
+// 1) création de la grille centrale 
+function generateGridTab(n,m){
+    
+    var variable_entrante = "#txtCj";
+    var gridTab = new Array();
+
+    for(var i = 0; i< m; i++){
+        gridTab[i] = new Array(n);
+        for(var j = 0; j< n; j++){
+            gridTab[i][j] = $(variable_entrante + i + "_" + j).val();
         }
     }
+    return gridTab;
+}
 
-    // ---------------- calcul du critère 1 --------------------/
-	var critere_1 = Math.max.apply(Math, tab_Cj).toString();
-	critere_1 = (tab_Cj.indexOf(critere_1)); // je récupère l'index de mon critere 1 dans le tableau pour pouvoir l'utiliser dans mon tableau de travail 
-    // var index_critere_1 = tab_Cj.indexOf(critere_1.toString());
-    // var variable_critere1 = "x" + (index_critere_1 + 1);
-    // console.log("Mot premier critère:" + critere_1);
-    // console.log("La variable du 1er critère:" + variable_critere1);
-	// ------------ fin calcul du critère 1 --------------------//
-    console.log(tab_Cj);
-    return critere_1; // 
+// 2) création de la table Z
+function generateCjTab(n,m){
+
+    var variable_z = "#variable_z";
+    var cjTab = new Array();
+    var nb_colonne = parseInt(n) + parseInt(m);
+
+    for(var i =0; i < nb_colonne; i++){
+        cjTab[i] = $(variable_z + i).val();
+    }
+    for(var i =0; i < nb_colonne; i++){
+        if( cjTab[i] === undefined){
+            cjTab[i] = "0";
+        };
+    }
+    return cjTab;
+}
+
+// 3) création de la table Q
+function generateQTab(m){
+
+    var variable_contrainte = "#valeur_contrainte_";
+    var qTab = [];
+
+    for(var i =0; i < m; i++){
+        qTab[i] = $(variable_contrainte + i).val();
+    }
+    return qTab;
 }
 
 
-// ---- calcul critere 2 
-function minRatio() {
+// 3-1) Création du tableau cpCz
+function generateCpXjTab(cpTab, gridTab,n,m){
 
-    //console.log("mon retour de fonction" + calculCj());
+    var nb_colonne = parseInt(n) + parseInt(m);
+    var j = 0;
+    var resultat = 0;
+    var cpXjTab = new Array();
+    while(j <nb_colonne){
+        resultat = 0;
+        for(var i =0; i< m; i++){
+             resultat = resultat + (cpTab[i] * gridTab[i][j]);
+        }
+        cpXjTab.push(resultat);
+        j++;  
+    }
+    return cpXjTab;
+}
 
-    var nbContraintes = $("#txtNbContraintes").val();
-    var critere_1 = calculCj();
-    var i;
-    var critere_2 = "0";
-    // var ratio;
-    var ratios = [];
-    // var min;
+//4) Création du tableau Cp 
+function generateCpTab(m){
+    var cpTab = [];
+    for(var i = 0; i<m; i++)
+        cpTab[i] = "0";
+    return cpTab;
+} 
 
-    //calculer 
+// 5) Ajout des variables sortantes 
+function addOutputVariables(gridTab, n, m, outpoutVariablesTab){
 
-    for (i = 0; i < nbContraintes; i++) {
+        for(var i = 0; i < m; i++){
+            for(var j = 0; j<m; j++){
+                gridTab[i].push(outpoutVariablesTab[i][j]);
+            }
+        }
+    return gridTab;
+        
+}
 
-        ratios.push($("#valeur_contrainte_" + i).val() / $("#txtCj" + i + "_" + critere_1).val(), i);
+// 6) Tableau générant les variables sortantes 
+function generateOutputVariables(n,m){
 
+    var outputVariables = [];
+
+    for(var i = 0; i < m; i++){
+        outputVariables[i] = new Array(n)
+        for(var j = 0; j < n; j++){
+            outputVariables[i][i] = '1';
+        }
     }
     
-    critere_2 = Math.min.apply(Math, ratios);
-    return critere_2; 
-    // console.log(Math.min.apply(Math, ratios));
+    for (var i = 0; i < m; i++) {
+        for (var j = 0; j < m; j++) {
+            if (outputVariables[i][j] != '1') {
+                outputVariables[i][j] = "0";
+            }
+        }
+    }    
+    return outputVariables
 }
 
-// --- Construire tableau de matrice
+// 7) Trouve le critère 1 
+// Retourne l'index / colonne 
+function findFirstCritere(cjTab){
 
-function buildBaseTable() {
-    var nbVariables = parseInt($("#txtNbVariables").val());
-    var nbContraintes = parseInt($("#txtNbContraintes").val());
-    var nbVariablesAjoutees = countInfSign();
-    var nbColonnes = nbVariablesAjoutees + nbVariables;
-    var nbLignes = nbContraintes;
-    var ii = nbVariables;
+    var val_of_max_value =  Math.max.apply(Math, cjTab).toString(); // la valeur max
+    var index_of_first_critere = (cjTab.indexOf(val_of_max_value)); // l'index de la valeur max 
+    return index_of_first_critere;
+}
 
+// 8 ) Trouve le critere 2 
+// Retourne l'index / ligne 
+function findSecondCritere(qTab, gridTab, index_of_first_critere, m){
+    
+    for(var i = 0; i < m ; i++){
+        var ratio = qTab[i] /  gridTab[i][index_of_first_critere];
 
-    // on cree le tableau bleu, contenant les lignes
-    var grille = new Array();
-
-    // on cree les lignes (tableau vert) les unes après les autres
-    for (var i = 0; i < nbLignes; i++)
-        grille[i] = new Array(nbColonnes);
-
-    // on parcourt les lignes...
-    for (var i = 0; i < nbLignes; i++)
-        // ... et dans chaque ligne, on parcourt les cellules
-        for (var j = 0; j < nbColonnes; j++)
-            grille[i][j] = $("#txtCj" + i + "_" + j).val();
-
-
-    //ajout de la variables xf1 xf2 xf3 etc ... 
-    for (var i = 0; i < nbLignes; i++) {
-        //ajouter variable si signe inférieur pour mettre sous forme linéaire
-        if ($("#selection_comparateur" + i + " option:selected").text() == "<=") {
-            grille[i][ii] = "1";
-        } else {
-            grille[i][ii] = "0";
-        }
-        ii++;
-    }
-
-
-    //ajout du 0 pour les variables n'ayant pas de valeur (coefficient)
-    for (var i = 0; i < nbLignes; i++) {
-        for (var j = 0; j < nbColonnes; j++) {
-            if (grille[i][j] === undefined) {
-                grille[i][j] = "0";
+        for(var j = 0; j<m ; j++){
+            if (ratio < (qTab[i] / gridTab[j][index_of_first_critere])){
+               var min_ratio = ratio;
+               var index_of_second_critere = i;
             }
         }
     }
-
-    return grille;
-
+    return index_of_second_critere;
 }
 
-// compte le nombre de signe inférieur =< 
-function countInfSign() {
+// 9) Retourne la valeur pivot 
+function getPivotValue(gridTab, index_of_first_critere, index_of_second_critere ){
+    return gridTab[index_of_second_critere][index_of_first_critere];
+}
 
-    var nbContraintes = parseInt($("#txtNbContraintes").val());
-    nb_inegalite_inf = 0;
+// 10) Mettre à jour le tableau Cp 
+function updateCpTab(cpTab, index_of_second_critere, valueOfPivot){
+    cpTab[index_of_second_critere] = valueOfPivot;
 
-    for (i = 0; i < nbContraintes; i++) {
-        if ($("#selection_comparateur" + i + " option:selected").text() == "<=") {
-            nb_inegalite_inf++;
+    return cpTab;
+}
+
+
+
+// 11) Creation du tableau Cj - Zj 
+function generateCpZjTab(cjTab, cpXjTab, m, n){
+   
+    var nb_colonne =  parseInt(n) + parseInt(m);
+    var cjZjTab = new Array();
+
+    for(var i =0; i<nb_colonne; i++){
+       if (cjTab[i] - cpXjTab[i]){
+        cjZjTab.push(cjTab[i] - cpXjTab[i]);
+       } else{
+        cjZjTab.push(0);
+       }
+       
+    }
+    return cjZjTab;
+}
+
+// 12) Vérifier si valeur supérieur à 0 dans cjZjTab
+function verifyValuesOfCpCz(cjZjTab, n, m){
+
+    var nb_colonne =  parseInt(n) + parseInt(m);
+    
+    for(var i =0; i<nb_colonne; i++){
+        if(cjZjTab[i] > 0){
+            return true; 
+        }
+      }
+    return false;
+}
+
+// 13) MAJ des TABS 
+function majAlltabs(gridTab, index_of_second_critere, index_of_first_critere,valueOfPivot, m,n){
+  
+   var nb_colonne =  parseInt(n) + parseInt(m);
+   var test;
+ 
+   for(var i = 0; i < m; i++){
+       for(var j =0; j<nb_colonne; j++){
+           if(i == index_of_second_critere){
+            gridTab[i][j] = gridTab[i][j] / valueOfPivot;
+           }
         }
     }
-    return nb_inegalite_inf;
+   
+//    for(i = 0; i < m; i++){
+//         for(var j =0; j<nb_colonne; j++){
+//             if((gridTab[i][index_of_first_critere] != "0") && (i !== index_of_second_critere)){
+            
+//                 test = gridTab[i][j] - (gridTab[i][index_of_first_critere] * gridTab[index_of_second_critere][j]);
+                
+//                 // gridTab[i][j] = test;
+                
+//              }
+            
+            
+// }
+  
+// }
+return gridTab;
 }
 
-//ajoute à ma grille de base la colonne Q
-function getColonneQ(grille){
+function majOthersLines(gridTab, index_of_second_critere, index_of_first_critere,valueOfPivot, m,n){
+    var nb_colonne =  parseInt(n) + parseInt(m);
+    for(i = 0; i < m; i++){
+                for(var j =0; j<nb_colonne; j++){
+                    if((gridTab[i][index_of_first_critere] != "0") && (i !== index_of_second_critere)){
+                    
+                        gridTab[i][j] = gridTab[i][j] - (gridTab[i][index_of_first_critere] * gridTab[index_of_second_critere][j]);
+                       
+                     }
+                     return gridTab;
 
-    var nbContraintes = $("#txtNbContraintes").val();
-    var nbVariables = parseInt($("#txtNbVariables").val());
-    var nbVariablesAjoutees = countInfSign();
-    var nbColonnes = nbVariablesAjoutees + nbVariables;
-    var nbLignes = nbContraintes;
 
-    for (i = 0; i < nbLignes; i++){
-        grille[i][nbColonnes+1] = $("#valeur_contrainte_" + i).val();
-        alert($("#valeur_contrainte_" + i).val());
+}
     }
-    
-    return grille;
-
 }
+
+
+
+
+
+
+
 
